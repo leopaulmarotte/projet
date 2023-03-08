31e72215bd6c5274141046f699bc84d47084f25f
lmarotte <leo-paul.marotte@ensae.fr>
2023-03-08 14:46:19 +0000

    ok

--- tests/test_s1q4_graph_loading2.py
+++ tests/test_s1q4_graph_loading2.py
@@ -1,1 +1,18 @@
 # À compléter
+
+
+import sys 
+sys.path.append("delivery_network/")
+
+import unittest 
+from graph import Graph, graph_from_file
+
+class Test_GraphCC(unittest.TestCase):
+    def test_network4(self):
+            g = graph_from_file("input/network.04.in")
+            self.assertEqual(g.nb_nodes, 10)
+            self.assertEqual(g.nb_edges, 4)
+            self.assertEqual(g.graph[1][0][2], 6)
+
+if __name__ == '__main__':
+    unittest.main()
