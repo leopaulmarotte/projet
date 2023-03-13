3c086f45e4d7b58efaf044c4893ebe54eff0c6cd
lmarotte <leo-paul.marotte@ensae.fr>
2023-03-13 20:56:00 +0000

    opk

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -1,8 +1,26 @@
+class union_find:
+
+    def __init__(self, parent_node = {}):
+        self.parent_node = parent_node
+
+    def make_set(self, u):
+        for i in u:
+            self.parent_node[i] = i
+
+    def find(self, k):
+        if self.parent_node[k] == k:
+            return k
+        return self.find(self.parent_node[k])
+
+    def op_union(self, a, b):
+        x = self.find(a)
+        y = self.find(b)
+        self.parent_node[x] = y
+
+
 class Graph:
-   
 
     def __init__(self, nodes=[]):
-      
         self.nodes = nodes
         self.graph = dict([(n, []) for n in nodes])
         self.nb_nodes = len(nodes)
@@ -10,7 +28,6 @@ class Graph:
     
 
     def __str__(self):
-        """Prints the graph as a list of neighbors for each node (one per line)"""
         if not self.graph:
             output = "The graph is empty"            
         else:
@@ -18,28 +35,28 @@ class Graph:
             for source, destination in self.graph.items():
                 output += f"{source}-->{destination}\n"
         return output
-    
-    def add_edge(self, node1, node2, power_min, dist=1):
-       
-        if node1 not in self.graph:
-            self.graph[node1] = []
-            self.nb_nodes +=1
-        if node2 not in self.graph:
-            self.graph[nodes2] = []
-            self.nb_nodes +=1
-        
-        self.nb_edges +=1
-        self.graph[node1].append((node2, power_min, dist))
-        self.graph[node2].append((node1, power_min, dist))
-       
 
     
+    
+    def add_edge(self, node1, node2, power_min, dist=1):
+        if node1 not in self.graph:
+            self.graph[node1] = []
+            self.nb_nodes += 1
+            self.nodes.append(node1)
+        if node2 not in self.graph:
+            self.graph[node2] = []
+            self.nb_nodes += 1
+            self.nodes.append(node2)
+
+        self.graph[node1].append((node2, power_min, dist))
+        self.graph[node2].append((node1, power_min, dist))
+        self.nb_edges += 1
+
 
     def get_path_with_power(self, src, dest, power):
         visited_nodes = {node : False for node in self.nodes}
         visited_nodes[src] = True
-
-            
+   
         def finding_a_path(node, path):
             if node == dest:
                 return path
@@ -54,40 +71,35 @@ class Graph:
 
         return finding_a_path(src, [src])
 
-  
-   
-
-
+        raise NotImplementedError
     
-        
+
     def connected_components(self):
-        liste_composante = []
-        noeud_visite = {noeud:False for noeud in self.nodes}
+        components_list = []
+        visited_nodes = {node : False for node in self.nodes}
 
-        def dfs(noeud):
-            composante = [noeud]
-            for voisin in self.graph[noeud]:
-                voisin = voisin[0]
-                if not noeud_visite[voisin]:
-                    noeud_visite[voisin] = True
-                    composante += dfs(voisin)
-            return composante
+        def exploration(node):
+            component = [node]
+            for neighbor in self.graph[node]:
+                neighbor = neighbor[0]
+                if not visited_nodes[neighbor]:
+                    visited_nodes[neighbor] = True
+                    component += exploration(neighbor)
+            return component
         
-        for noeud in self.nodes:
-            if not noeud_visite[noeud]:
-                liste_composante.append(dfs(noeud))
+        for node in self.nodes:
+            if not visited_nodes[node]:
+                components_list.append(exploration(node))
 
-        return liste_composante
-            
+        return components_list
 
-        
+        raise NotImplementedError
 
 
     def connected_components_set(self):
-    
-
         return set(map(frozenset, self.connected_components()))
     
+
     def min_power(self, src, dest):
         power_list = []
 
@@ -103,8 +115,6 @@ class Graph:
                     right = middle
             return(path,L[left])
 
-
-
         for node in self.nodes:
             for neighbor in self.graph[node]:
                 power_list.append(neighbor[1])
@@ -116,156 +126,110 @@ class Graph:
         else:
             return binary_search(self,power_list)
 
-
-
     def Dijkstra(self,src,dest,power):
-        infini = 10^9
-        distance_list = [infini for node in self.nodes]
-        distance_list[src] = 0
-        predecessor = [infini for node in self.nodes]
+        infinity = 1000000000000
+        distance_dic = {node : infinity for node in self.nodes}
+        distance_dic[src] = 0
+        predecessor_dic = {None : node for node in self.nodes}
 
         def finding_min(non_reached_nodes):
-            mini = infini
-            vertex = -1
+            mini = infinity
+            min_dts_node = -1
             for node in non_reached_nodes:
-                if distance_list[node] < mini:
-                    mini = distance_list[node]
-                    vertex = node
-            return(vertex)
+                if distance_dic[node] < mini:
+                    mini = distance_dic[node]
+                    min_dts_node = node
+            if min_dts_node == -1:
+                return None
+            else:
+                return min_dts_node
 
-        def dist(node1,node2):
-            for neighbor in self.graph[node1]:
-                if neighbor[0] == node2:
-                    return neighbor[2]
-            return("Erreur")
+        def power_and_dist_between_nodes(self,node1,node2):
+            for node in self.graph[node1]:
+                if node[0] == node2:
+                    return(node[1],node[2])
 
-        def power_fct(node1,node2):
-            for neighbor in self.graph[node1]:
-                if neighbor[0] == node2:
-                    return neighbor[1]
-            return("Erreur")
+        def update_distances(node1,node2):
+            power_of_edge,dist_of_edge = power_and_dist_between_nodes(self, node1, node2)
+            if (distance_dic[node2] > distance_dic[node1] + dist_of_edge) and (power >= power_of_edge):
+                distance_dic[node2] = distance_dic[node1] + dist_of_edge
+                predecessor_dic[node2] = node1
 
-        def distance_update(node1,node2):
-            if (distance_list[node2] > distance_list[node1] + dist(node1,node2)) and (power_fct((node1),node(2)) < power) :
-                distance_list[node2] = distance_list[node1] + dist(node1,node2)
-                predecessor[node2] = node1
-
-        my_non_reached_node = (self.nodes).remove(src)
-        while my_non_reached_node != []:
-            node_min = finding_min(my_non_reached_node)
-            my_non_reached_node.remove(node_min)
-            for neighbor in self.graph[node_min]:
-                neighbor = neighbor[0]
-                distance_update(node_min, neighbor)
+        my_non_reached_nodes = [node for node in self.nodes]
+        while my_non_reached_nodes != []:
+            nearest_neighbor = finding_min(my_non_reached_nodes)
+            if nearest_neighbor == None:
+                my_non_reached_nodes = []
+            else:
+                my_non_reached_nodes.remove(nearest_neighbor)
+                for neighbor_of_nearest_neighbor in self.graph[nearest_neighbor]:
+                    neighbor_of_nearest_neighbor = neighbor_of_nearest_neighbor[0]
+                    update_distances(nearest_neighbor, neighbor_of_nearest_neighbor)
+        shortest_path = []
         moving_node = dest
-        min_path = []
         while moving_node != src:
-            min_path = [moving_node] + min_path
-            moving_node = predecessor[moving_node]
-        min_path = [src] + min_path
-        return(min_path)
+            if moving_node == None:
+                return("Pas de chemin possible")
+            shortest_path.insert(0,moving_node)
+            moving_node = predecessor_dic[moving_node]
+        shortest_path.insert(0,src)
+        return(shortest_path)
+
+    def kruskal(self):
+
+        def del_occur(L):
+            LL = []
+            for i in L:
+                if not i in LL:
+                    LL.append(i)
+            return(LL)
+        
+        nb_nodes = self.nb_nodes
+        mst = Graph(range(1,nb_nodes+1))
+        
+        mst_union_find = union_find({})
+        mst_union_find.make_set(list(self.nodes))
+        edge_list = []
+        for node1 in self.nodes:
+            for node2 in self.graph[node1]:
+                node2,power_1_2 = node2[0],node2[1]
+                edge_list.append([power_1_2,max(node1,node2),min(node1,node2)])
+        edge_list_sorted_unic = del_occur(sorted(edge_list))
+        final_edge_list = []
+        for edge in edge_list_sorted_unic:
+            edge.reverse()
+            final_edge_list.append(edge)
+        
+        for edge in final_edge_list:
+            node1,node2,power = edge
+            if mst_union_find.find(node1) != mst_union_find.find(node2):
+                mst.add_edge(node1, node2, power)
+                mst_union_find.op_union(node1, node2)
+        
+        return(mst)
+    
+    def f1(self, src, dest):
+        g = self.kruskal()
+        t = g.min_power(src, dest)
+        return t
+
 
 
 def graph_from_file(filename):
-    
+
     with open(filename, "r") as file:
-    
-            n, m = map(int, file.readline().split())
-            g = Graph(range(1, n+1))
-            for _ in range(m):
-                edge = list(map(int, file.readline().split()))
-                if len(edge) == 3:
-                    node1, node2, power_min = edge
-                    g.add_edge(node1, node2, power_min) # will add dist=1 by default
-                elif len(edge) == 4:
-                    node1, node2, power_min, dist = edge
-                    g.add_edge(node1, node2, power_min, dist)
-                else:
-                    raise Exception("Format incorrect")
+        n, m = map(int, file.readline().split())
+        g = Graph(range(1, n+1))
+        for _ in range(m):
+            edge = list(map(int, file.readline().split()))
+            if len(edge) == 3:
+                node1, node2, power_min = edge
+                g.add_edge(node1, node2, power_min) # will add dist=1 by default
+            elif len(edge) == 4:
+                node1, node2, power_min, dist = edge
+                g.add_edge(node1, node2, power_min, dist)
+            else:
+                raise Exception("Format incorrect")
     return g
 
-class DisjointSet:
-    parent = {}
- 
-    # effectue l'opération MakeSet
-    def makeSet(self, n):
-        # créer `n` ensembles disjoints (un pour chaque sommet)
-        for i in range(1,n+1):
-            self.parent[i] = i
- 
-    # Trouver la racine de l'ensemble auquel appartient l'élément `k`
-    def find(self, k):
-        # si `k` est racine
-        if self.parent[k] == k:
-            return k
- 
-        # se reproduisent pour le parent jusqu'à ce que nous trouvions la racine
-        return self.find(self.parent[k])
- 
-    # Perform Union de deux sous-ensembles
-    def union(self, a, b):
-        # trouver la racine des ensembles auxquels appartiennent les éléments `x` et `y`
-        x = self.find(a)
-        y = self.find(b)
- 
-        self.parent[x] = y
- 
- 
-# Fonction pour construire MST en utilisant l'algorithme de Kruskal
-def Kruskal(g):
- 
-    # stocke les arêtes présentes dans MST
-    MST = []
-    n = g.nb_nodes
-    print(n)
-    # Initialise la classe `DisjointSet`.
-    # Créer un ensemble singleton pour chaque élément de l'univers.
-    ds = DisjointSet()
-    ds.makeSet(n)
- 
-    index = 0
 
-    edges = []
-    for i in g.nodes :
-        for t in g.graph[i]:
-        
-        
-            edges.append((t[1],min([i,t[0]]),max([i,t[0]])))
-        
-    
-    
-    F = frozenset(edges)
-    edges1 = list(F)
-    edges1 = sorted(edges1)
-    
-    edgesf = []
-    for i in range(len(edges1)):
-        edges2 = list(edges1[i])
-        
-        edges2.reverse()
-        
-        edgesf.append(edges2)
-    print(edgesf)
-        
-  
- 
-    #there are n nodes, so our MST must have n - 1 edges
-    while len(MST) != n - 1:
- 
-        # considère le bord suivant avec un poids minimum du graph
-        (src, dest, power) = edgesf[index]
-        index = index + 1
- 
-        # 
-        x = ds.find(src)
-        y = ds.find(dest)
- 
-        # si les deux points de terminaison ont des parents différents, ils appartiennent à
-        # différents composants connectés et peuvent être inclus dans MST
-        if x != y:
-            MST.append((src, dest, power))
-            ds.union(x, y)
- 
-    return MST
- 
-    
--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,4 +1,4 @@
-from graph import Graph, graph_from_file, Kruskal
+from graph import Graph, graph_from_file
 import time
 from time import perf_counter
 
@@ -6,28 +6,33 @@ from time import perf_counter
 data_path = "input/"
 file_name = "routes.2.in"
 
-#with open(data_path + file_name, "r") as file:
- #   src = []
- #   dest = []
- #    
- #   a = map(int, file.readline().split())
 
- #   for i in range(10):
- #       n,m,c = map(int, file.readline().split())
-  #      src.append(n)
-   #     dest.append(m)
-    #
+with open(data_path + file_name, "r") as file:
+    src = []
+    dest = []
+     
+    a = map(int, file.readline().split())
 
-g = graph_from_file("input/" + "network.00.in")
+    for i in range(10):
+        n,m,c = map(int, file.readline().split())
+        src.append(n)
+        dest.append(m)
+    print(src, dest)
+    
 
-#t1 = time.perf_counter()
-#
-#for i in range(10):
- #   g.min_power(src[i], dest[i])
+g = graph_from_file("input/" + "network.2.in")
 
-#t2 = time.perf_counter()
-#print(list(a)[0]* (t2 - t1))
+t1 = time.perf_counter()
+
+#for i in range(3):
+    
+g.kruskal(src[1],dest[1])
 
 
 
-Kruskal(g)
+t2 = time.perf_counter()
+print((list(a)[0])* (t2 - t1))
+
+
+
+
