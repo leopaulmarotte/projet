62fde3f2638310fc71966afcb7302a64f9775089
lmarotte <leo-paul.marotte@ensae.fr>
2023-04-06 16:25:53 +0000

    ok

--- tests/test_q10.py
+++ tests/test_q10.py
@@ -8,4 +8,4 @@ from time import perf_counter
 
 print(time_estimation(2))
 
-# We do it for the second routes, and we obtain an estimated time to calculate all the min_power of 123876 seconds
+# We do it for the second route, and we obtain an estimated time to calculate all the min_power of 123876 seconds
