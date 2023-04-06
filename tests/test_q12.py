70f86c91276c7beee15eebd6e79d8b355e50e179
lmarotte <leo-paul.marotte@ensae.fr>
2023-04-06 16:03:30 +0000

    ok

--- tests/test_q12.py
+++ tests/test_q12.py
@@ -7,7 +7,7 @@ import time
 from time import perf_counter
 
 
-#on crée un grapghe nous-mêmes car les graghes donnés sont soit trop simples, soit trop compliqués
+# we create a simple graph to easily test the kruskal function
 
 g2 = Graph([1, 2, 3, 4, 5])
 
@@ -23,48 +23,23 @@ print(kruskal(g2))
 
 g = graph_from_file("input/network.2.in")
 
-#résultat cohérent
+# we can easily verify that the result is correct
+# and it is
 
 
-#g1 = graph_from_file("input/network.9.in")
+g1 = graph_from_file("input/network.9.in")
 
 t11 = time.perf_counter()
-#print(g1) # Quel que soit le volume du fichier, le graphe kruskal s'affiche en quelques secondes
+print(g1) #
 t22 = time.perf_counter()
-#print(t22-t11) # 10,44 secondes pour calculer le kruskal
+print(t22-t11) 
 
-def new_time_estimation(n):
-    with open("input/routes." + str(n) + ".in","r") as file:
-        time_est = 0
-        a = int(file.readline()) # We save the amount of itineraries
-        g = graph_from_file("input/network." + str(n) + ".in")
-        g1 = kruskal(g)
-        for i in range(10): # Average with 10 itineraries
-            node1,node2,p = map(int, file.readline().split())
-            t1 = time.perf_counter()
-            #print(t1)
-            opti = new_minpower_aux(g1, node1, node2)
-            t2 = time.perf_counter()
-            time_est += (t2-t1)
-    print((a/10)*time_est)       
-
-def new_time_estimation(n):
-    with open("input/routes." + str(n) + ".in","r") as file:
-        time_est = 0
-        a = int(file.readline()) # We save the amount of itineraries
-        g = graph_from_file("input/network." + str(n) + ".in")
-        g1 = kruskal(g)
-        for i in range(10): # Average with 10 itineraries
-            node1,node2,p = map(int, file.readline().split())
-            t1 = time.perf_counter()
-            #print(t1)
-            opti = new_minpower_aux(g1, node1, node2)
-            t2 = time.perf_counter()
-            time_est += (t2-t1)
-    print((a/10)*time_est)  
-
-new_time_estimation(4)
-# on met au max 1 seconde pour calculer la puissance minimale qu'il faut pour un trajet donné, quelle que soit la base de données.
+t111 = time.perf_counter()
+print(kruskal(g1))
+t222 = time.perf_counter()
+print(t22-t11)
+print(t222 - t111)      # it takes 0.5 more seconds to display the kruskal of a graph than to 
+                        # just display the graph, which can be considered quick enough
 
 
 
