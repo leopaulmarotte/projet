d5280eff2b66d8d21d53e99413aae599f6d99d82
lmarotte <leo-paul.marotte@ensae.fr>
2023-04-06 09:53:08 +0000

    ok

--- delivery_network/graphe2.py
+++ delivery_network/graphe2.py
@@ -62,3 +62,19 @@ class Graph:
 
 
 
+def graph_from_file(filename):
+
+    with open(filename, "r") as file:
+        n, m = map(int, file.readline().split())
+        g = Graph(range(1, n+1))
+        for _ in range(m):
+            edge = list(map(int, file.readline().split()))
+            if len(edge) == 3:
+                node1, node2, power_min = edge
+                g.add_edge(node1, node2, power_min) # will add dist=1 by default
+            elif len(edge) == 4:
+                node1, node2, power_min, dist = edge
+                g.add_edge(node1, node2, power_min, dist)
+            else:
+                raise Exception("Format incorrect")
+    return g
