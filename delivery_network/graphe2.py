1c2f30ecb56b942b9e2b950072fb94fa34305226
lmarotte <leo-paul.marotte@ensae.fr>
2023-04-06 10:08:44 +0000

    ok

--- delivery_network/graphe2.py
+++ delivery_network/graphe2.py
@@ -6,6 +6,7 @@ class Graph:
     def add_edge(self, node1, node2, weight):
         self.m_graph.append([node1, node2, weight])
 
+    # Finds the root node of a subtree containing node `i`
     def find_subtree(self, parent, i):
         if parent[i] == i:
             return i
@@ -23,7 +24,6 @@ class Graph:
             parent[yroot] = xroot
             subtree_sizes[xroot] += 1
 
-        
     def kruskals_mst(self):
         # Resulting tree
         result = []
@@ -36,6 +36,7 @@ class Graph:
         # Sort edges by their weight
         self.m_graph = sorted(self.m_graph, key=lambda item: item[2])
 
+        # Auxiliary arrays
         parent = []
         subtree_sizes = []
 
@@ -44,8 +45,11 @@ class Graph:
             parent.append(node)
             subtree_sizes.append(0)
 
-    
+        # Important property of MSTs
+        # number of egdes in a MST is
+        # equal to (m_num_of_nodes - 1)
         while e < (self.m_num_of_nodes - 1):
+            # Pick an edge with the minimal weight
             node1, node2, weight = self.m_graph[i]
             i = i + 1
 
@@ -57,10 +61,10 @@ class Graph:
                 result.append([node1, node2, weight])
                 self.connect_subtrees(parent, subtree_sizes, x, y)
 
+        # Print the resulting MST
         for node1, node2, weight in result:
             print("%d - %d: %d" % (node1, node2, weight))
-
-
+        return result
 
 def graph_from_file(filename):
 
